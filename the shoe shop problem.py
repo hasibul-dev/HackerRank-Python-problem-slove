00cbf4d969ba4081bdd30fcd2f28cbe6c8a24d2e
Md Hasibul Hassan Shanto <157917726+shanto812@users.noreply.github.com>
2026-04-19 01:41:02 +0600

    the shoe shop problem

---
+++ the shoe shop problem.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+num_shoes = int(input())
+
+shoe_sizes = map(int, input().split())
+inventory = Counter(shoe_sizes)
+
+num_customers = int(input())
+
+total_earned = 0
+
+for _ in range(num_customers):
+    size, price = map(int, input().split())
+    
+    if inventory[size] > 0:
+        total_earned += price    
+        inventory[size] -= 1      
+
+print(total_earned)
